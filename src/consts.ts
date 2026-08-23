61f957e90bcfcf22379a13fe607ac22df5bb8009
Daniel Rovira <danielroviratorres@gmail.com>
2026-08-23 17:04:40 -0300

    Atualiza o conteúdo e o layout do site para refletir a nova identidade do "Guia Prático", incluindo melhorias na estrutura do cabeçalho, rodapé e páginas, além de remover estilos obsoletos.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Guia Prático';
+export const SITE_DESCRIPTION = 'Soluções práticas, ferramentas gratuitas e guias passo a passo para facilitar sua rotina. Simplifique o seu dia a dia com o Guia Prático.';
